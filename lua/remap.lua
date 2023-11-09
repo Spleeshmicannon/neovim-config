add3c12e3e9e00cba4fcf50aac45b952d0c9c3ae
J. M. Benson <jackmartinbenson@gmail.com>
2023-11-09 19:40:38 +1000

    some basic improvements

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>gb', ":GitBlameToggle <CR>")
 vim.keymap.set('n', '<leader>to', ":NvimTreeOpen <CR>")
 vim.keymap.set('n', '<leader>tc', ":NvimTreeClose <CR>")
+vim.keymap.set('n', '<leader>tm', ":bel split term://bash <CR>")
