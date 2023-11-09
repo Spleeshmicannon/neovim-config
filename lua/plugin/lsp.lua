2120ccacb97d6cdb4f5ba705b5d2497815e0cc7c
J. M. Benson <jackmartinbenson@gmail.com>
2023-11-09 22:23:49 +1000

    Update lsp.lua

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -1,20 +1,37 @@
 local lsp_zero = require('lsp-zero')
 
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        "pyright",
+        "cmake",
+        "clangd",
+        "lua_ls",
+    },
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
+
+
+
+
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
-    "pyright",
-    "cmake",
-    "clangd",
-    "lua_ls",
-  },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+lsp_zero.format_on_save({
+    format_opts = {
+        async = false,
+        timeout_ms = 100,
+    },
+    servers = {
+        ["pyright"] = { 'python' },
+        ["clangd"] = { 'cpp', 'c' },
+        ["lua_ls"] = { 'lua' }
+    }
 })
+
+lsp_zero.setup_servers({ "pyright", 'cmake', 'clangd', 'lua_ls' })
