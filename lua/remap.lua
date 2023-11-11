d2a979ba1b738dfa6889931ce2927f6be92f7b3c
spleeshmicannon <jackmartinbenson@gmail.com>
2023-11-11 13:09:20 +1000

    matching patterns

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,3 +3,10 @@ vim.keymap.set('n', '<leader>gb', ":GitBlameToggle <CR>")
 vim.keymap.set('n', '<leader>to', ":NvimTreeOpen <CR>")
 vim.keymap.set('n', '<leader>tc', ":NvimTreeClose <CR>")
 vim.keymap.set('n', '<leader>tm', ":bel split term://bash <CR>")
+
+
+-- matchings
+vim.keymap.set('i', '{', '{}<left>')
+vim.keymap.set('i', '[', '[]<left>')
+vim.keymap.set('i', "'", "''<left>")
+vim.keymap.set('i', '"', '""<left>')
