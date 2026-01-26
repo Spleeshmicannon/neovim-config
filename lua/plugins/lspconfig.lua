0a031a16287b3bbf58f0ec4f051bca324ef40b0a
jbenson <jackmartinbenson@gmail.com>
2026-01-26 17:11:10 +1000

    fixed some lsp issues

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,25 +5,31 @@ local languages = {
     "lua_ls",
     "rust_analyzer",
     "taplo",
-    "zls"
+    "zls",
+    "bazelrc_lsp"
 }
 
 return {
     {
-    	"neovim/nvim-lspconfig",
+        "neovim/nvim-lspconfig",
         dependencies = { "saghen/blink.cmp" },
-    	config = function()
+        config = function()
             local blink = require("blink.cmp")
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = blink.get_lsp_capabilities(capabilities)
-
-    		vim.lsp.config("*", {
+            
+            local default_config = {
                 capabilities = capabilities,
                 on_attach = require("keymaps").on_attach
-            })
-
-    		vim.lsp.enable(languages)
-    	end,
+            }
+            
+            -- Configure each LSP server explicitly
+            for _, lsp in ipairs(languages) do
+                vim.lsp.config(lsp, default_config)
+            end
+            
+            vim.lsp.enable(languages)
+        end,
     },
     {
         'williamboman/mason.nvim',
---
+++ lua/plugins/plugin-man.lua
@@ -0,0 +1,6 @@
+return {
+    "spleeshmicannon/plugin-finder.nvim",
+    dir = "/home/jbenson/Development/maintained_projects/plugin-finder.nvim",
+    lazy = false,
+    opts = {}
+}
